ba3ef7ae41250cc9d0c808e3b263345daf9c6851
Andreas <hamburger1984@gmail.com>
2020-12-08 19:27:52 +0100

    trying to fix plasma-disks and kded

--- pkgs/development/libraries/kde-frameworks/kded.nix
+++ pkgs/development/libraries/kde-frameworks/kded.nix
@@ -1,16 +1,28 @@
 {
-  mkDerivation, lib, propagate,
-  extra-cmake-modules, kdoctools,
+  mkDerivation, lib, propagate, wrapGAppsHook,
+  extra-cmake-modules, glib, gtk2, gtk3, gsettings-desktop-schemas,
+  kdoctools,
   kconfig, kcoreaddons, kcrash, kdbusaddons, kinit, kservice, qtbase,
 }:
 
 mkDerivation {
   name = "kded";
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
-  nativeBuildInputs = [ extra-cmake-modules kdoctools ];
+  nativeBuildInputs = [ extra-cmake-modules wrapGAppsHook kdoctools ];
+  dontWrapGApps = true;
   buildInputs = [
     kconfig kcoreaddons kcrash kdbusaddons kinit kservice qtbase
+    gsettings-desktop-schemas glib gtk2 gtk3
   ];
+  cmakeFlags = [
+    "-DGTK2_GLIBCONFIG_INCLUDE_DIR=${glib.out}/lib/glib-2.0/include"
+    "-DGTK2_GDKCONFIG_INCLUDE_DIR=${gtk2.out}/lib/gtk-2.0/include"
+    "-DGLIB_SCHEMAS_DIR=${gsettings-desktop-schemas.out}/"
+  ];
+  preConfigure = ''
+    NIX_CFLAGS_COMPILE+=" -DGSETTINGS_SCHEMAS_PATH=\"$GSETTINGS_SCHEMAS_PATH\""
+  '';
+
   outputs = [ "out" "dev" ];
   setupHook = propagate "out";
 }
