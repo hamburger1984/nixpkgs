7bcea3257f78dfe216df27c6e86c316c8570b655
Andreas <hamburger1984@gmail.com>
2020-12-28 15:37:59 +0100

    kio: add dependency kded

--- pkgs/development/libraries/kde-frameworks/kio/default.nix
+++ pkgs/development/libraries/kde-frameworks/kio/default.nix
@@ -2,7 +2,7 @@
   mkDerivation, lib,
   extra-cmake-modules, kdoctools, qttools,
   karchive, kbookmarks, kcompletion, kconfig, kconfigwidgets, kcoreaddons,
-  kdbusaddons, ki18n, kiconthemes, kitemviews, kjobwidgets, knotifications,
+  kdbusaddons, kded, ki18n, kiconthemes, kitemviews, kjobwidgets, knotifications,
   kservice, ktextwidgets, kwallet, kwidgetsaddons, kwindowsystem, kxmlgui,
   qtbase, qtscript, qtx11extras, solid, kcrash
 }:
@@ -12,7 +12,7 @@ mkDerivation {
   meta = { maintainers = [ lib.maintainers.ttuegel ]; };
   nativeBuildInputs = [ extra-cmake-modules kdoctools ];
   buildInputs = [
-    karchive kconfigwidgets kdbusaddons ki18n kiconthemes knotifications
+    karchive kconfigwidgets kdbusaddons kded ki18n kiconthemes knotifications
     ktextwidgets kwallet kwidgetsaddons kwindowsystem qtscript qtx11extras
     kcrash
   ];
