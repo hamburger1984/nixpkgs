d6d476f839e06f474573c2b6921e89a952564e67
Andreas <hamburger1984@gmail.com>
2021-01-22 20:47:05 +0100

    kguiaddons: add dependencies

--- pkgs/development/libraries/kde-frameworks/kguiaddons.nix
+++ pkgs/development/libraries/kde-frameworks/kguiaddons.nix
@@ -1,13 +1,13 @@
 { mkDerivation
 , extra-cmake-modules
-, qtbase, qtx11extras, wayland
+, qtbase, qtx11extras, libXdmcp, wayland,
 }:
 
 mkDerivation {
   name = "kguiaddons";
 
   nativeBuildInputs = [ extra-cmake-modules ];
-  buildInputs = [ qtx11extras wayland ];
+  buildInputs = [ qtx11extras libXdmcp wayland ];
   propagatedBuildInputs = [ qtbase ];
 
   outputs = [ "out" "dev" ];
