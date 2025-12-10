bd10ad366014ea7b809c99ef2ee34cf18b6a1057
Andreas K. <hamburger1984@gmail.com>
2025-12-10 21:16:47 +0100

    Switch to my (hamburger1984) fork of zha-device-handlers (incorporates the fix below) Switch to https://github.com/tomuk5/zha-device-handlers/tree/_TZ3000_isw9u95y for tuya temperature and humidity sensor fix.

--- pkgs/development/python-modules/zha-quirks/default.nix
+++ pkgs/development/python-modules/zha-quirks/default.nix
@@ -19,10 +19,17 @@ buildPythonPackage rec {
   disabled = pythonOlder "3.12";
 
   src = fetchFromGitHub {
-    owner = "zigpy";
+    #owner = "zigpy";
+    owner = "hamburger1984";
+    #owner = "tomuk5";
     repo = "zha-device-handlers";
-    tag = version;
-    hash = "sha256-vVA8FmNLzatfClRTvb9pZm5SQxgSQh+iAllOl+OIo6Q=";
+    #tag = version;
+    #hash = "sha256-vVA8FmNLzatfClRTvb9pZm5SQxgSQh+iAllOl+OIo6Q=";
+    hash = "sha256-lCcJCQ+/X3GpZPG3Li9f+sspFG+f2Ju3R6523jR1U2w=";
+    ##tag = version;
+    rev = "196b569507a18e7d3fdc2e65cc26734cfae91ffc";
+    #rev = "ad330c53ab2473828c563ab12aa518b60fa69e38";
+    #hash = "sha256-9NS8kPSZ6tNU+M7s5eJ6j+okTwX04eZ3S8dpkXw9V7w=";
   };
 
   postPatch = ''
